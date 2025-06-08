5887c316a4a33c23033b53a86416aba548350588
North-river1015 <s10k2024+1@outlook.com>
2025-06-08 16:59:35 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Robust
+
+A browser that only shows (relatively) reliable sites. 
+
+https://north-river1015.github.io/Robust/
---
+++ backend/express.js
@@ -0,0 +1,35 @@
+//Backend. Recieves query from frontend and sends it to Google Custom Search API, 
+//which is set to only include reliable websites.
+
+//I want to thank https://shamsfiroz.medium.com/building-a-full-stack-javascript-search-engine-with-google-custom-search-api-cad796360d2c 
+require('dotenv').config();
+
+const express = require('express')
+const app = express()
+const axios = require('axios');
+const cors = require('cors');
+
+app.use(cors());
+
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+const GOOGLE_CSE_ID = process.env.GOOGLE_CSE_ID;
+
+app.get('/search', async (req, res) => {
+  const { q } = req.query;
+  if (!q) return res.status(400).json({ error: 'Enter a query (*_*)' });
+
+try {
+  const apiUrl = `https://www.googleapis.com/customsearch/v1?key=${AIzaSyDXK0pwGH6IWSofHZNBenZezNXSKc52BZE}}&cx=${60434bec15f0b4b80}&q=${encodeURIComponent(q)}`;
+  const { data } = await axios.get(apiUrl);
+  res.json({ results: data.items || [] });
+
+  } catch (e) {
+    res.status(500).json({ error: 'Search failed', details: e.message });
+  }
+});
+
+app.listen(3001, () => {
+  console.log('Server running at http://localhost:3001');
+});
+
+
